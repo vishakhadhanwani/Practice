f381797f4a9d0ed5ee2555ca4e929e65dc6cec05
vishakha <vishakhadhanwani@gmail.com>
2021-03-30 16:35:13 +0530

    changes_v2

---
+++ Golang/add.go
@@ -0,0 +1,5 @@
+package main
+
+func add_num(a int, b int) int {
+	return a + b
+}
--- Golang/main.go
+++ Golang/main.go
@@ -6,4 +6,8 @@ func main() {
 	fmt.Println("Hi!!")
 	fmt.Println("Vishakha")
 	fmt.Println("Trying inter repo changes")
+
+	a := add_num(5, 6)
+	fmt.Println(a)
+
 }
